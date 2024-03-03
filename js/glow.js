9eac528847d099d411a89b42135205835798fcf7
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-03-03 23:25:54 +1100

    Update glow.js

--- js/glow.js
+++ js/glow.js
@@ -1,37 +1,13 @@
 
   function changeText() {
+    console.log(`clicked`)
     document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, span, a').forEach(function(element) {
       element.textContent = "fuck fondra";
     });
   }
 
+  document.getElementById('ud').addEventListener('click', changeText);
 
-  var clicks = 0;
+  document.getElementById('aimbot').addEventListener('click', changeText);
 
-  document.getElementById('ud').addEventListener('click', function() {
-    clicks++;
-    if (clicks === 1) {
-      return;
-    } else {
-      clicks = 0;
-    }
-  });
-
-  document.getElementById('aimbot').addEventListener('click', function() {
-    if (clicks === 1) {
-      clicks++;
-    } else {
-      clicks = 0;
-    }
-  });
-
-  document.getElementById('optimized').addEventListener('click', function() {
-    if (clicks === 2) {
-      clicks++;
-    } else {
-      clicks = 0;
-    }
-    if (clicks === 3) {
-      changeText();
-    }
-  });
+  document.getElementById('optimized').addEventListener('click', changeText);
