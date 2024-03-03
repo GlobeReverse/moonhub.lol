1fb94f02b848b1ce0f0e4f82d099d82940f3333d
Globe <92859545+GlobeReverse@users.noreply.github.com>
2024-03-03 23:28:16 +1100

    Update glow.js

--- js/glow.js
+++ js/glow.js
@@ -1,6 +1,5 @@
 
   function changeText() {
-    console.log(`clicked`)
     document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, span, a').forEach(function(element) {
       element.textContent = "fuck fondra";
     });
